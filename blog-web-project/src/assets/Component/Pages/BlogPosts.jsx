ebc011385a5c04e56bbe48c16980a0d00b7c6371
kabiraminu50 <kabiraminu50@gmail.com>
2025-11-16 11:17:47 -0800

    modified my blogPost logic

--- blog-web-project/src/assets/Component/Pages/BlogPosts.jsx
+++ blog-web-project/src/assets/Component/Pages/BlogPosts.jsx
@@ -61,8 +61,8 @@ posts.length === 0 ?(<p>No Blog Post Available</p>
 
   posts.map((post) => (
  <div key={post._id} className='blog-item'>
-  {post.image &&  (
-<img src={`http://localhost:8000/uploads/${post.image}`} alt={post.title} 
+  {post.imageUrl &&  (
+<img src={post.imageUrl} alt={post.title} 
 
 style={{width:"300px",borderRadius:"10px"}}
 />
@@ -72,7 +72,7 @@ style={{width:"300px",borderRadius:"10px"}}
   <p>{post.subtitle}</p>
   <p>{post.content}</p>
   <small>{new Date(post.createdAt).toDateString()}</small>
-<button onClick={()=> handleDelete(posts._id)}>Delete</button>
+<button onClick={()=> handleDelete(post._id)}>Delete</button>
 
 </div>
 
